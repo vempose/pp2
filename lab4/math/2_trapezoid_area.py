050203fa7025199cf5f465e4f6537f8ae60d67e4
Alexandr C. <vempose@gmail.com>
2025-02-21 15:50:27 +0500

    Clean up some things

--- lab4/math/2_trapezoid_area.py
+++ lab4/math/2_trapezoid_area.py
@@ -1,9 +1,3 @@
-# Height: 5
-# Base, first value: 5
-# Base, second value: 6
-# Expected Output: 27.5
-
-
 h = float(input("Height: "))
 a = float(input("Base, first value: "))
 b = float(input("Base, second value: "))
